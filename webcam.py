a02a98c2342f1da6e3937180c367e7b52b7eb58c
Nicholas Sargeant <nicholassargeant@gmail.com>
2021-10-21 19:04:36 +1100

    Updated error handling, displays in grey
    Handles error if frame not grabbed, changed escape key to 'q', and gave messages for the user explaining what is happening. Also displaying in greyscale now

--- webcam.py
+++ webcam.py
@@ -1,18 +1,37 @@
 # File for opening webcam
 import cv2
+# import apriltag
 
+print("Opening webcam, please wait...")
 capture = cv2.VideoCapture(0)
-
 # Ensure that the webcam was opened correctly
 if not capture.isOpened():
     raise IOError("Cannot open webcam")
 
+print("Webcam opened, press 'q' to quit")
+
+
 while True:
     ret, frame = capture.read()
-    frame = cv2.resize(frame, None, fx = 0.9, fy = 0.9, interpolation = cv2.INTER_AREA)
-    cv2.imshow('Webcam', frame)
+    # If frame was read correctly, then ret will be True
+    if not ret:
+        print("Cannot receive frame")
+        raise IOError("Webcam frame not received")
 
-    # Check if esc was pressed
-    c = cv2.waitKey(1)
-    if c == 27:
+    # Perform some operation on the frame here
+    # Resize the frame
+    frame = cv2.resize(frame, None, fx = 0.9, fy = 0.9, interpolation = cv2.INTER_AREA)
+    # Convert the size to grey
+    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+
+    # Display frame
+    cv2.imshow('Webcam', grey)
+
+    # Check if 'q' was pressed
+    key = cv2.waitKey(1)
+    if key == ord('q'):
         break
+
+capture.release()
+cv2.destroyAllWindows()
