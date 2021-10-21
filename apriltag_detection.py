94a0360c23a057b1b467d7dabb9e87ca159cf4cb
NickSarge <nicholassargeant@gmail.com>
2021-10-21 23:33:37 +1100

    Create apriltag_detection.py
    Basic apriltag detecting from images using command line input python apriltag_detection.py --image images/example_imgx.png

---
+++ apriltag_detection.py
@@ -0,0 +1,47 @@
+# apriltag reading
+import cv2
+import apriltag
+import argparse
+
+# For parsing command line arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, 
+    help = "Path to AprilTag image")
+args = vars(ap.parse_args())
+
+# Image and convert it to grayscale
+print("Loading image...")
+img = cv2.imread(args["image"])
+grey_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+print("Looking for AprilTag/s from the 36h11 family...")
+options = apriltag.DetectorOptions(families="tag36h11")
+detector = apriltag.Detector(options)
+results = detector.detect(grey_img)
+print("{} AprilTags successfully detected".format(len(results)))
+
+# loop over the AprilTag detection results
+for r in results:
+    # extract the bounding box (x, y)-coordinates for the AprilTag
+    # and convert each of the (x, y)-coordinate pairs to integers
+    (ptA, ptB, ptC, ptD) = r.corners
+    ptB = (int(ptB[0]), int(ptB[1]))
+    ptC = (int(ptC[0]), int(ptC[1]))
+    ptD = (int(ptD[0]), int(ptD[1]))
+    ptA = (int(ptA[0]), int(ptA[1]))
+    # draw the bounding box of the AprilTag detection
+    cv2.line(img, ptA, ptB, (0, 255, 0), 2)
+    cv2.line(img, ptB, ptC, (0, 255, 0), 2)
+    cv2.line(img, ptC, ptD, (0, 255, 0), 2)
+    cv2.line(img, ptD, ptA, (0, 255, 0), 2)
+    # draw the center (x, y)-coordinates of the AprilTag
+    (cX, cY) = (int(r.center[0]), int(r.center[1]))
+    cv2.circle(img, (cX, cY), 5, (0, 0, 255), -1)
+    # draw the tag family on the image
+    tagFamily = r.tag_family.decode("utf-8")
+    cv2.putText(img, tagFamily, (ptA[0], ptA[1] - 15),
+        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    print("Tag family: {}".format(tagFamily))
+# show the output image after AprilTag detection
+cv2.imshow("Image", img)
+cv2.waitKey(0)
