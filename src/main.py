8417c74e2e4fa8ce131f5c54f7c9d8abc3e21825
Nathan Dugdale <nath23725652@gmail.com>
2021-11-08 15:05:55 +1100

    feat: single tag 8 demo

--- src/main.py
+++ src/main.py
@@ -20,17 +20,17 @@ from visualisation import camera_pose_visualizer as cpv
 
 if __name__ == "__main__":
     
-    # Clear atlas
-    atlas_folder = './pose/atlas'
-    for filename in os.listdir(atlas_folder):
-        file_path = os.path.join(atlas_folder, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception as e:
-            print('Failed to delete %s. Reason: %s' % (file_path, e))
+    # # Clear atlas
+    # atlas_folder = './pose/atlas'
+    # for filename in os.listdir(atlas_folder):
+    #     file_path = os.path.join(atlas_folder, filename)
+    #     try:
+    #         if os.path.isfile(file_path) or os.path.islink(file_path):
+    #             os.unlink(file_path)
+    #         elif os.path.isdir(file_path):
+    #             shutil.rmtree(file_path)
+    #     except Exception as e:
+    #         print('Failed to delete %s. Reason: %s' % (file_path, e))
 
     # For parsing command line arguments
     parser = argparse.ArgumentParser()
