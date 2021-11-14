ac1c596a298f39e790db09cdde7649cc08813eb1
Josh Wilkinson <joshlwilkinson9@gmail.com>
2021-11-14 21:13:20 +1100

    Add more documentation

--- src/camera/webcam.py
+++ src/camera/webcam.py
@@ -1,7 +1,16 @@
 # File for opening webcam
 import cv2
 
+
 def open_webcam():
+    """open the first webcam connected to the computer to obtain the video stream for the project
+
+    Raises:
+        IOError: if no webcam can be opened
+
+    Returns:
+        capture stream: the stream of video from which images can be obtained
+    """
     print("Opening webcam, please wait...")
     capture = cv2.VideoCapture(0)
     # Ensure that the webcam was opened correctly
@@ -11,7 +20,19 @@ def open_webcam():
     print("Webcam opened, press 'q' to quit")
     return capture
 
+
 def get_current_webcam_frame(capture):
+    """get the current frame from the capture stream specified
+
+    Args:
+        capture (stream): the stream from which to grab a frame
+
+    Raises:
+        IOError: if a frame cannot be read from the specified stream
+
+    Returns:
+        image: the frame that was successfully read from the stream
+    """
     ret, frame = capture.read()
     # If frame was read correctly, then ret will be True
     if not ret:
@@ -20,9 +41,10 @@ def get_current_webcam_frame(capture):
     # Perform some operation on the frame here
     return frame
 
+
 if __name__ == "__main__":
-    
-    capture =  open_webcam()
+
+    capture = open_webcam()
 
     while True:
         # Get frame
